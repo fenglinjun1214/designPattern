226e2864a2fe91b69d56a94a431e2498c255523e
hejun1 <hejun1@tuhu.cn>
2021-08-14 23:44:20 +0800

    111

--- demo/src/main/java/com/example/demo/Singleton.java
+++ demo/src/main/java/com/example/demo/Singleton.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.sun.xml.internal.ws.util.CompletedFuture;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class Singleton {
 
-    private static volatile Singleton singleton = null;
+    private static /*volatile*/ Singleton singleton = null;
 
     private Singleton() {
 
@@ -24,24 +31,92 @@ public class Singleton {
 
 
     public static void main(String[] args) {
-        Singleton singleton = Singleton.getInstance();
 
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1000);
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            fixedThreadPool.execute(new Runnable() {
+        Executor executor = initExecutor();
+        /*for (int i = 0; i < 50; i++) {
+            int finalI = i;
+            executor.execute(() -> {
 
-                @Override
-                public void run() {
-                    try {
-                        System.out.println(index);
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    Singleton singleton = Singleton.getInstance();
+                    System.out.println(finalI + "i========" + singleton);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             });
+        }*/
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<CompletableFuture<Integer>> s = list.stream().map(v->getRemark(v)).collect(Collectors.toList());
+        s.stream().forEach(v->{
+            v.thenApply((result)->{
+                System.out.println("thenApply res" +result);
+                return result;
+            });
+        });
+        //使用allof 获得返回值
+        CompletableFuture cf = CompletableFuture.allOf(s.toArray(new CompletableFuture[s.size()]));
+
+        try {
+            System.out.println("date1" + new Date());
+
+            CompletableFuture<List<Integer>> ssss = cf.thenApply(v-> s.stream().map(mm ->mm.join()).collect(Collectors.toList()));
+            System.out.println("date2" + new Date());
+
+            List<Integer> lista = ssss.get();
+            System.out.println("date3" + new Date());
+
+            lista.stream().forEach(v->{
+                System.out.println(v);
+            });
+        } catch (Exception e) {
+
         }
+
+    }
+
+    public static CompletableFuture<Integer> getRemark(Integer v) {
+        Executor executor = initExecutor();
+
+        CompletableFuture<Integer> completedFuture = CompletableFuture.supplyAsync(()-> {
+            System.out.println("开始date" + new Date());
+            try {
+                Thread.sleep(30000);
+
+            } catch(Exception e ) {
+
+            }
+                    System.out.println("threadName" + Thread.currentThread().getName());
+            System.out.println("date" + new Date());
+                    System.out.println("v is " + v);
+                    System.out.println("supplyAsync 是否为守护线程 " + Thread.currentThread().isDaemon());
+            return v;
+        } , executor);
+
+        return completedFuture;
+    }
+
+
+    public static Executor initExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        // 核心线程池大小
+        executor.setCorePoolSize(10);
+        // 最大线程数
+        executor.setMaxPoolSize(50);
+        // 队列容量
+        executor.setQueueCapacity(20);
+        // 活跃时间
+        executor.setKeepAliveSeconds(30);
+        // 线程名字前缀
+        executor.setThreadNamePrefix("excel.executor-");
+
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+        executor.initialize();
+        return executor;
     }
 
 
